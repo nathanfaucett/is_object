5e037413d27e2f49b682ead0e508ba1a0c21ed3a
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-30 12:49:38 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-module.exports = function isObject(obj) {
+module.exports = isObject;
+
+
+function isObject(obj) {
     var type = typeof(obj);
     return type === "function" || (obj && type === "object") || false;
-};
+}
